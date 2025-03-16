742da89db0f7d703eaac9ef7c97bf1a2aa3b791e
PasqualeCelani <thespeedkiller01@gmail.com>
2025-03-16 09:45:22 +0100

    Layout have been seted

---
+++ app/frontend/src/layouts/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Layout =({children}) =>{
+    return(
+       <html lang="en">
+        <head></head>
+        <body>
+            {children}
+        </body>
+       </html>
+    )
+}
+
+export default Layout;
