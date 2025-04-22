db2363c161b0fb781cf305180485773780868286
PasqualeCelani <thespeedkiller01@gmail.com>
2025-04-22 22:23:18 +0200

    Quaility distribution plot have been implemented

--- verification/plot.py
+++ verification/plot.py
@@ -1,6 +1,7 @@
 import argparse
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 
 def plot_FRR_FAR(name):
     data = pd.read_csv(name+".csv")
@@ -28,6 +29,17 @@ def plot_ROC(name):
     plt.ylabel("1-FRR")
     plt.show()
 
+def plot_Quaility_distribution(name):
+    data = pd.read_csv(name+".csv")
+    plt.figure(figsize=(8, 5))
+    sns.kdeplot(data['image'], fill=True, bw_adjust=0.2, clip=(0, 1))
+    plt.title("Quality Measure Distribution")
+    plt.xlabel("Quality Score (0 to 1)")
+    plt.ylabel("Probability Density")
+    plt.grid(True)
+    plt.show()
+
+
 def main():
     parser = argparse.ArgumentParser()
 
@@ -37,15 +49,17 @@ def main():
     args = parser.parse_args()
 
     type = args.type
-    if not (type  in ["ROC", "FRR-FAR"]):
-        raise Exception("The type must be either ROC or FRR-FAR")
+    if not (type  in ["ROC", "FRR-FAR", "QualityDistribution"]):
+        raise Exception("The type must be either ROC, FRR-FAR or QualityDistribution")
     
     benchmark_file = args.benchmark_file
 
     if(type == "ROC"):
         plot_ROC(benchmark_file)
-    else:
+    elif(type == "FRR-FAR"):
         plot_FRR_FAR(benchmark_file)
+    else:
+        plot_Quaility_distribution(benchmark_file)
 
 
 if __name__ == "__main__":
